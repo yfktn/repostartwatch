6c5c514b10b753312f0b47c07983056a31aaf5fe
yfktn <friskantoni@gmail.com>
2018-03-26 15:53:30 +0700

    Jalankan secara concurent

--- main.go
+++ main.go
@@ -57,18 +57,31 @@ func main() {
 		writeHelp()
 		os.Exit(1)
 	}
+	resultC, errorC := make(chan string), make(chan error)
+
 	repos := make([]repo, len(args)-1)
 	for i := 1; i < len(args); i++ {
 		repos[i-1].url = fmt.Sprintf("https://github.com/%s", args[i])
-		fmt.Println("Menganalisa ", repos[i-1].url)
 	}
 	// run
 	for _, v := range repos {
-		r, e := v.setRepoValueBasedOn(v.getPage())
-		if r {
-			fmt.Println(v.toString())
-		} else {
-			fmt.Println("Gagal memproses: ", v.url, " E: ", e)
+		go func(r repo) {
+			_, err := r.setRepoValueBasedOn(r.getPage())
+			if err != nil {
+				errorC <- err
+			} else {
+				msg := r.toString()
+				resultC <- msg
+			}
+		}(v)
+	}
+	for i := 1; i <= len(repos); i++ {
+		select {
+		case oke := <-resultC:
+			fmt.Println(oke)
+		case err := <-errorC:
+			fmt.Println("Gagal:", err)
 		}
 	}
+
 }

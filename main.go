23f9e166ae35cfc3b659952cadb564253c8490e2
yfktn <friskantoni@gmail.com>
2018-03-26 10:43:30 +0700

    Tambahkan fungsi testing dan juga untuk melakukan parsing terhadap html menggunakan paket soup

--- main.go
+++ main.go
@@ -4,20 +4,63 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/anaskhan96/soup"
 )
+
+type repo struct {
+	url        string
+	start      int
+	watch      int
+	lastCommit string
+}
 
 func writeHelp() {
 	fmt.Println("repovalue.exe golang/go golang/proposal")
 }
 
+func (r repo) getPage() string {
+	b, e := soup.Get(r.url)
+	if e != nil {
+		fmt.Println("Failed to get ", r.url, ":", e)
+		return ""
+	}
+	return b
+}
+
+func (r *repo) setRepoValueBasedOn(page string) (bool, error) {
+	doc := soup.HTMLParse(page)
+	elem := doc.Find("ul", "class", "pagehead-actions")
+	aSocialCount := elem.FindAll("a", "class", "social-count")
+	var e error
+	r.watch, e = strconv.Atoi(strings.TrimSpace(aSocialCount[0].Text()))
+	if e != nil {
+		return false, e
+	}
+	r.start, e = strconv.Atoi(strings.TrimSpace(aSocialCount[1].Text()))
+	if e != nil {
+		return false, e
+	}
+	return true, nil
+}
+
+func (r repo) toString() string {
+	return "URL: " + r.url + " watch: " + string(r.watch) + " start: " + string(r.start)
+}
+
 func main() {
-	repos := os.Args
-	if len(repos) < 2 {
+	args := os.Args
+	if len(args) < 2 {
 		writeHelp()
 		os.Exit(1)
 	}
-	for i := 1; i < len(repos); i++ {
-		fmt.Println("Menganalisa ", repos[i])
+	repos := make([]repo, len(args)-1)
+	for i := 1; i < len(args); i++ {
+		repos[i-1].url = fmt.Sprintf("https://github.com/%s", args[i])
+		fmt.Println("Menganalisa ", repos[i-1].url)
+
 	}
 
 }
---
+++ main_test.go
@@ -0,0 +1,65 @@
+// main_test.go
+package main
+
+import (
+	"testing"
+)
+
+var ToTest string = `<ul class="pagehead-actions">
+  <li>
+      <a href="/login?return_to=%2Fyfktn%2Fyii-floating-fixed" class="btn btn-sm btn-with-count tooltipped tooltipped-n" aria-label="You must be signed in to watch a repository" rel="nofollow">
+    <svg class="octicon octicon-eye" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
+    Watch
+  </a>
+  <a class="social-count" href="/yfktn/yii-floating-fixed/watchers" aria-label="1 user is watching this repository">
+    1
+  </a>
+
+  </li>
+
+  <li>
+      <a href="/login?return_to=%2Fyfktn%2Fyii-floating-fixed" class="btn btn-sm btn-with-count tooltipped tooltipped-n" aria-label="You must be signed in to star a repository" rel="nofollow">
+    <svg class="octicon octicon-star" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path></svg>
+    Star
+  </a>
+
+    <a class="social-count js-social-count" href="/yfktn/yii-floating-fixed/stargazers" aria-label="1 user starred this repository">
+      1
+    </a>
+
+  </li>
+
+  <li>
+      <a href="/login?return_to=%2Fyfktn%2Fyii-floating-fixed" class="btn btn-sm btn-with-count tooltipped tooltipped-n" aria-label="You must be signed in to fork a repository" rel="nofollow">
+        <svg class="octicon octicon-repo-forked" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>
+        Fork
+      </a>
+
+    <a href="/yfktn/yii-floating-fixed/network" class="social-count" aria-label="0 users forked this repository">
+      0
+    </a>
+  </li>
+</ul>
+
+      <h1 class="public ">
+  <svg class="octicon octicon-repo" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
+  <span class="author" itemprop="author"><a class="url fn" rel="author" href="/yfktn">yfktn</a></span><!--
+--><span class="path-divider">/</span><!--
+--><strong itemprop="name"><a data-pjax="#js-repo-pjax-container" href="/yfktn/yii-floating-fixed">yii-floating-fixed</a></strong>
+
+</h1>`
+
+func TestPemberianNilai(t *testing.T) {
+	var toTestR repo
+	toTestR.url = "TesterUseVar"
+	_, e := toTestR.setRepoValueBasedOn(ToTest)
+	if e != nil {
+		t.Fatal("Terdapat masalah membaca", e)
+	}
+	if toTestR.watch != 1 {
+		t.Fatalf("Nilai watch harusnya 1 tapi dapat %d", toTestR.watch)
+	}
+	if toTestR.start != 1 {
+		t.Fatalf("Nilai start harusnya 1 tapi dapat %d", toTestR.start)
+	}
+}
